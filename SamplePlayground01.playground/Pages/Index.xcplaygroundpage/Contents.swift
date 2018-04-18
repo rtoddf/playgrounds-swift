03c477505d9c6e247a1b26897194d581fbeb4f79
Todd Fleeman <Todd.Fleeman@coxinc.com>
2018-04-18 15:27:43 -0400

    first playground

---
+++ SamplePlayground01.playground/Pages/Index.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+/*:
+ 
+# Heading
+ ## Heading 2
+ ### Heading 3
+
+Text
+
+ [CNN](https:www.cnn.com)
+ 
+ [Page 1](Page1)
+ 
+ ### List with Bullets
+ * Line 1
+    * Sub 1
+    * sub 2
+ * Line 2
+ 
+ ### List with Numbers
+ 1. item 1
+ 2. item 2
+ 
+ ![Iceman](comics-iceman.jpg)
+ 
+ */
+
+/*:
+ Lorem ipsum dolor sit amet, *consectetur adipiscing elit*. Quisque tristique tincidunt mauris, a laoreet eros dignissim sit amet. In vitae velit eleifend lacus **convallis suscipit eget non nisi**. Sed eget ullamcorper libero, commodo ornare risus. Donec at eleifend nunc, at mattis ante. Nulla odio nunc, elementum eget libero at, egestas consectetur turpis. Cras bibendum felis massa, a tincidunt mauris pharetra quis. Nulla suscipit tortor risus, et fringilla magna tincidunt sed. Donec fermentum ut mi sit amet tincidunt. Aenean imperdiet ultricies rhoncus. 
+ */
+
+var i: Int = 0
