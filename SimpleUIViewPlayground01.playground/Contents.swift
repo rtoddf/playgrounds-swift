9850676f5585901517129275369c9702ba8064d0
Todd Fleeman <Todd.Fleeman@coxinc.com>
2018-04-23 10:08:04 -0400

    catching up

---
+++ SimpleUIViewPlayground01.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+import PlaygroundSupport
+
+let view = UIView()
+view.backgroundColor = .orange
+view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+
+PlaygroundPage.current.liveView = view
