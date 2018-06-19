307694399ca638f6c3d8f2169fb1ee4799fa951b
Tod Fleeman <rtoddf@me.com>
2018-06-19 10:18:30 -0400

    commenting out trial stuff to move on

--- MenuBoard.playground/Contents.swift
+++ MenuBoard.playground/Contents.swift
@@ -17,15 +17,15 @@ class MenuBoard:UIViewController {
         return view
     }()
     
-    var pizzaTextView:UITextView = {
-        let view = UITextView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.font = UIFont(name: "Chalkduster", size: 16.0)
-        view.textColor = .white
-        view.backgroundColor = .clear
-        view.isEditable = false
-        return view
-    }()
+//    var pizzaTextView:UITextView = {
+//        let view = UITextView()
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//        view.font = UIFont(name: "Chalkduster", size: 16.0)
+//        view.textColor = .white
+//        view.backgroundColor = .clear
+//        view.isEditable = false
+//        return view
+//    }()
     
     let imageView:UIImageView = {
         let iv = UIImageView()
@@ -37,39 +37,39 @@ class MenuBoard:UIViewController {
     }()
 
     var headingText = "Huli Pizza Company \n"
-    var menuText = ""
+//    var menuText = ""
     
     func addViews() {
         view.addSubview(imageView)
         view.addSubview(textView)
-        view.addSubview(pizzaTextView)
+//        view.addSubview(pizzaTextView)
         textView.text = headingText
-        pizzaTextView.text = menuText
+//        pizzaTextView.text = menuText
         
-//        var constraints:[NSLayoutConstraint] = []
-//        constraints += [NSLayoutConstraint.init(item: imageView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 0.0)]
-//        constraints += [NSLayoutConstraint.init(item: imageView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 0.0)]
-//        constraints += [NSLayoutConstraint.init(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: 0.0)]
-//        constraints += [NSLayoutConstraint.init(item: imageView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 0.3, constant: 0.0)]
-//
-//        constraints += [NSLayoutConstraint.init(item: textView, attribute: .top, relatedBy: .equal, toItem: imageView, attribute: .bottom, multiplier: 1.0, constant: 10.0)]
-//        constraints += [NSLayoutConstraint.init(item: textView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 20.0)]
-//        constraints += [NSLayoutConstraint.init(item: textView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: -20.0)]
-//        constraints += [NSLayoutConstraint.init(item: textView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1.0, constant: 150.0)]
-//
+        var constraints:[NSLayoutConstraint] = []
+        constraints += [NSLayoutConstraint.init(item: imageView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 0.0)]
+        constraints += [NSLayoutConstraint.init(item: imageView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 0.0)]
+        constraints += [NSLayoutConstraint.init(item: imageView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: 0.0)]
+        constraints += [NSLayoutConstraint.init(item: imageView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 0.3, constant: 0.0)]
+
+        constraints += [NSLayoutConstraint.init(item: textView, attribute: .top, relatedBy: .equal, toItem: imageView, attribute: .bottom, multiplier: 1.0, constant: 10.0)]
+        constraints += [NSLayoutConstraint.init(item: textView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 20.0)]
+        constraints += [NSLayoutConstraint.init(item: textView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: -20.0)]
+        constraints += [NSLayoutConstraint.init(item: textView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1.0, constant: 150.0)]
+
 //        constraints += [NSLayoutConstraint.init(item: pizzaTextView, attribute: .top, relatedBy: .equal, toItem: textView, attribute: .bottom, multiplier: 1.0, constant: 10.0)]
 //        constraints += [NSLayoutConstraint.init(item: pizzaTextView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 20.0)]
 //        constraints += [NSLayoutConstraint.init(item: pizzaTextView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: -20.0)]
 //        constraints += [NSLayoutConstraint.init(item: pizzaTextView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1.0, constant: 100.0)]
+
+        view.addConstraints(constraints)
+        
+//        let imageHeight = view.frame.height * 0.25
 //
-//        view.addConstraints(constraints)
-        
-        let imageHeight = view.frame.height * 0.25
-        
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": imageView]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": textView]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": pizzaTextView]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0(\(imageHeight))]-4-[v1(100)][v2(150)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": imageView, "v1": textView, "v2": pizzaTextView]))
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": imageView]))
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": textView]))
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": pizzaTextView]))
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0(\(imageHeight))]-4-[v1(100)][v2(150)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": imageView, "v1": textView, "v2": pizzaTextView]))
     }
     
     func printHeading(_ newText:String) {
@@ -77,10 +77,10 @@ class MenuBoard:UIViewController {
         textView.text = headingText
     }
     
-    func printMenu(_ newText:String) {
-        menuText += newText + "\n"
-        pizzaTextView.text = menuText
-    }
+//    func printMenu(_ newText:String) {
+//        menuText += newText + "\n"
+//        pizzaTextView.text = menuText
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -99,16 +99,14 @@ menuBoard.navigationController?.isNavigationBarHidden = true
 PlaygroundPage.current.liveView = navigationController
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-
 menuBoard.printHeading("Aloha Pizza")
 menuBoard.printHeading("Our Pizzas Today")
-
 
 let list = ["Pepperoni", "Sausage", "Cheese", "Huli Chicken", "Surfboard Vegan", "Deep Diver", "Big Island"]
 let prices = [10.95, 11.95, 8.15, 15.75, 13.95, 16.75, 21.95]
 
 for index in 0..<list.count {
-    menuBoard.printMenu("\(list[index])  \(prices[index])")
+    menuBoard.printHeading("\(list[index])  \(prices[index])")
 }
 
 
